39660603dfe09a6511db730ee14de0a7320f61e3
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-22 13:52:04 -0400

    Putting evertything inside the ready function, so I can use the selected amenities as filters

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -1,4 +1,4 @@
-const {$} = window;
+const { $ } = window;
 $(document).ready(function () {
   const myAmenities = {};
   let myList = [];
@@ -10,7 +10,7 @@ $(document).ready(function () {
     if (this.checked) {
       myAmenities[dataId] = dataName;
     } else {
-// sourcery skip: only-delete-object-properties
+      // sourcery skip: only-delete-object-properties
       delete (myAmenities[dataId]);
     }
     for (const key in myAmenities) {
@@ -20,52 +20,48 @@ $(document).ready(function () {
     $('div.amenities > h4').text(output);
     myList = [];
   });
-});
 
-$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
-  if (data.status === 'OK') {
-    $('div#api_status').addClass('available');
-  }
-  else {
-    $('div#api_status').removeClass('available');
-  }
-});
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
 
-//adding ajax 
-$.ajax({  //Send a post request from curl version into ajax jquery
-  type: 'POST',
-  url: 'http://0.0.0.0:5001/api/v1/places_search',
-  contentType: 'application/json; charset=utf-8',
-  dataType: 'json',
-  data: '{}',
-  success: function (data) {
-      for (const place of Object.values(data)) {  // Loop into the result of the request and create an article tag representing a Place in the section.places.
-          $('section.places').append(`<article>
-  <div class="title_box">
-    <h2>${place.name}</h2>
-    <div class="price_by_night">$${place.price_by_night}</div>
-  </div>
-  <div class="information">
-    <div class="max_guest">
-      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
-      </br>
-      ${place.max_guest} Guests
-    </div>
-    <div class="number_rooms">
-      <I class="fa fa-bed fa-3x" aria-hidden="true"></i>
-      </br>
-      ${place.number_rooms} Bedrooms
-    </div>
-    <div class="number_bathrooms">
-      <I class="fa fa-bath fa-3x" aria-hidden="true"></i>
-      </br>
-      ${place.number_bathrooms} Bathrooms
-    </div>
-  </div>
-  <div class="description">
-    ${place.description}
-  </div>
-</article>`);
+  const search = (filters = {}) => {
+    $.ajax({
+      type: 'POST',
+      url: 'http://localhost:5001/api/v1/places_search',
+      data: JSON.stringify(filters),
+      //dataType: 'json',
+      contentType: 'application/json',
+      success: function (data) {
+        $('SECTION.places').empty();
+        $('SECTION.places').append(data.map(place => {
+          return `<article>
+                    <div class="title_box">
+                      <h2>${place.name}</h2>
+                      <div class="price_by_night">$${place.price_by_night}</div>
+                    </div>
+                    <div class="information">
+                      <div class="max_guest">${place.max_guest} Guests</div>
+                      <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+                      <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+                    </div>
+                    <div class="description">
+                      ${place.description}
+                    </div>
+                  </article>`
+        }));
       }
-  }
+    });
+  };
+
+  $('button').click(function () {
+    const filters = {amenities: Object.keys(myAmenities)};
+    search(filters);
+  });
+
+  search();
 });
