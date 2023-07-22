0214a0902eb3ffb1a0161c186b660ac0990ada5a
AbigailCastroFigueroa <abigail.castro-figueroa@holbertonschool.com>
2023-07-21 20:30:06 -0400

    Changing the javascript file to the excercise

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,50 +1,71 @@
-$('document').ready(function () {
-  // when apis active turn red
-  const url = 'http://' + window.location.hostname + ':5001/api/v1/status/';
-  $.get(url, function (response) {
-    if (response.status === 'OK') {
-      $('DIV#api_status').addClass('available');
+const {$} = window;
+$(document).ready(function () {
+  const myAmenities = {};
+  let myList = [];
+  const checkbox = $('.amenities input[type="checkbox"]');
+  checkbox.prop('checked', false);
+  checkbox.change(function () {
+    const dataId = $(this).attr('data-id');
+    const dataName = $(this).attr('data-name');
+    if (this.checked) {
+      myAmenities[dataId] = dataName;
     } else {
-      $('DIV#api_status').removeClass('available');
+// sourcery skip: only-delete-object-properties
+      delete (myAmenities[dataId]);
     }
+    for (const key in myAmenities) {
+      myList.push(myAmenities[key]);
+    }
+    const output = myList.join(', ');
+    $('div.amenities > h4').text(output);
+    myList = [];
   });
-  // display check box on each item
-  const amenities = {};
-  $('INPUT[type="checkbox"]').change(function () {
-    if ($(this).is(':checked')) {
-      amenities[$(this).attr('data-id')] = $(this).attr('data-name');
-    } else {
-      delete amenities[$(this).attr('data-id')];
-    }
-    if (Object.values(amenities).length === 0) {
-      $('.amenities H4').html('&nbsp;');
-    } else {
-      $('.amenities H4').text(Object.values(amenities).join(', '));
-    }
-  });
+});
 
-  const url2 = 'http://' + window.location.hostname + ':5001/api/v1/places_search/';
-  $.ajax({
-    type: 'POST',
-    url: url2,
-    data: '{}',
-    dataType: 'json',
-    contentType: 'application/json',
-    sucess: function (data) {
-      $('section.places').append(data.map(place => {
-        return `<article>
-                  <div class="title_box">
-                    <h2>${place.name}</h2>
-                    <div class="price_by_night">${place.price_by_night}</div>
-                  </div>
-                  <div class="information">
-                    <div class="max_guest">${place.max_guest} Guests</div>
-                    <div class="number_rooms">${place.number_rooms} Bedrooms</div>
-                    <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
-                  </div>
-                  <div class="user"><b>Owner:</b> ${place.user.first_name} ${place.user.last_name}</div>
-                  <div class="description"> ${place.description}</div>
-                </article>`}));
+$.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+  if (data.status === 'OK') {
+    $('div#api_status').addClass('available');
+  }
+  else {
+    $('div#api_status').removeClass('available');
+  }
+});
+
+//adding ajax 
+$.ajax({  //Send a post request from curl version into ajax jquery
+  type: 'POST',
+  url: 'http://0.0.0.0:5001/api/v1/places_search',
+  contentType: 'application/json; charset=utf-8',
+  dataType: 'json',
+  data: '{}',
+  success: function (data) {
+      for (const place of Object.values(data)) {  // Loop into the result of the request and create an article tag representing a Place in the section.places.
+          $('section.places').append(`<article>
+  <div class="title_box">
+    <h2>${place.name}</h2>
+    <div class="price_by_night">$${place.price_by_night}</div>
+  </div>
+  <div class="information">
+    <div class="max_guest">
+      <i class="fa fa-users fa-3x" aria-hidden="true"></i>
+      </br>
+      ${place.max_guest} Guests
+    </div>
+    <div class="number_rooms">
+      <I class="fa fa-bed fa-3x" aria-hidden="true"></i>
+      </br>
+      ${place.number_rooms} Bedrooms
+    </div>
+    <div class="number_bathrooms">
+      <I class="fa fa-bath fa-3x" aria-hidden="true"></i>
+      </br>
+      ${place.number_bathrooms} Bathrooms
+    </div>
+  </div>
+  <div class="description">
+    ${place.description}
+  </div>
+</article>`);
       }
-    });
+  }
 });
